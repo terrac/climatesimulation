03b7f6c8f1c61d40feef8cbfa582988321f92e7d
terra <terra@terra-pc.exe>
2015-08-18 17:37:46 -0400

    u

--- climatesimulation/oldexamples/2/test3.js
+++ climatesimulation/oldexamples/2/test3.js
@@ -1,157 +1,3 @@
-
-
-
-
-gravity =function(){
-
-    // Get the vector pointing from the moon to the planet center
-    var moon_to_planet = new CANNON.Vec3();
-    this.position.negate(moon_to_planet);
-
-    // Get distance from planet to moon
-    var distance = moon_to_planet.norm();
-
-    // Now apply force on moon
-    // Fore is pointing in the moon-planet direction
-    moon_to_planet.normalize();
-    moon_to_planet.mult(1500/Math.pow(distance,2),this.force);
-    updateHeat(this);
-}
-
-
-
-//1 do some more testing 
-// a lots of small ground
-// b visibility and speed
-//
-//1 very high levels of friction only a few materials
-
-types = {}
-function addType(type,visibility,color,shape,mass,sleepSpeed,SleepTimeLimit){
-	types[type] = arguments
-}
-
-function useType(args){
-	var obj = new CANNON.Body({ mass: args.mass });
-	obj.addShape(args.shape);	
-    moon.preStep = gravity    
-    moon.allowSleep = true;
-    obj.sleepSpeedLimit = args.sleepSpeed;// Body will feel sleepy if speed<1 (speed == norm of velocity)
-    obj.sleepTimeLimit = args.sleepTimeLimit; // Body falls asleep after 1s of sleepiness
-  	world.add(moon);
-  	if(args.visibility)
-  		demo.addVisual(moon,new THREE.MeshLambertMaterial( { color: args.color } ));
-  	currentBody = moon;
-}
-layers = []
-function addLayer(intervalAfter,type,position,amount,random,params){
-	layers.push(arguments);
-}
-function useLayer(){
-	for(idx in layers){
-		args = layers[idx]
-		amount = 0;
-		setTimeout(function(){
-		    useType(types[type])
-		    randomFuncs[args.random](args.params)
-			if(amount < args.amount)
-				setTimeout(this,args.timeBetween)
-			amount++;	
-			
-	   },args.intervalAfter);
-	}
-	
-}
-var randomFuncs = {}
-randomFuncs.randomAllOver= function(shape){
-	currentBody.position.set(Math.random() * 5,Math.random() * 6-3,Math.random() * 6-3);
-}
-
-
-var atmosphereShape = new CANNON.Sphere(1);
-var heatShape = new CANNON.Sphere(0.1);
-var lightShape = new CANNON.Sphere(0.05);
-var planetShape = new CANNON.Sphere(1.5);
-var particleShape = new CANNON.Particle();
-addType("planet",false,null,planetShape,5,.1,0)
-addType("dirt",true,0x49311C,particleShape,1,.1,1)
-addType("greenery",true,0x00FF00,particleShape,1,.1,1)
-addType("ocean",true,0x0000FF,particleShape,1,.1,3)
-addType("atmosphere",false,null,atmosphereShape,.1,.1,3)
-addType("clouds",true,0xFFFFFF,particleShape,1,.1,3)
-
-
-var center = [0,0,0];
-addLayer("planet",0,center,1)
-addLayer("dirt",30,center,30,randomAllOver,{"shape":3});
-addLayer("greenery",200,center,30,randomAllOver,{"shape":3});
-addLayer("ocean",600,center,90,randomAllOver,{"shape":3});
-addLayer("atmosphere",2000,center,30,randomAllOver,{"shape":3});
-addLayer("clouds",3000,center,20,randomAllOver,{"shape":3});
-
-//1 initial layer planet
-//2 second layer dirt
-//3 third layer vegetation
-//4 fourth layer ocean  (basically all randomly positioned outside of earth to fall down)
-//  dirt/vegetation falls asleep instantly at a relatively high speed
-//  ocean falls asleep quickly
-//5 conductivity, if set a method is added which based on that number on a collide distributes heat
-//  based on it and the opposing conductivity
-//6 reflectivity, chance of light bouncing off or becoming heat, (the filter determines light going through )  
-//  
-//1 layers
-//2 restrictions
-//
-//1 add
-
-
-
-
-function addObject(type,visibility,color,shape,position,mass,colorFilter,conductivity,reflectivity,sleepSpeed,sleepTimeLimit){
-	var climate = {}
-	climate.heat = 1;
-	climate.conductivity = conductivity
-	
-	var obj = new CANNON.Body({ mass: mass });
-	obj.addShape(shape);
-	
-	moon.colorFilter = Math.random() > .7 ? greenFilter : blueFilter;	
-  	
-  	
-  	moon.addEventListener("collide",heatDistribute);
-  	//moon.velocity.set(0,0,8);
-    //moon.velocity.set(2,0,0);
-    //moon.linearDamping = 0.0;
-    moon.preStep = gravity
-    
-    moon.allowSleep = true;
-     
-    
-    obj.sleepSpeedLimit = sleepSpeed;// Body will feel sleepy if speed<1 (speed == norm of velocity)
-    obj.sleepTimeLimit = sleepTimeLimit; // Body falls asleep after 1s of sleepiness
-  	world.add(moon);
-  	if(visibility)
-  		demo.addVisual(moon,new THREE.MeshLambertMaterial( { color: color } ));
-}
-
-//a conductivity
-//b visibility (on/off)
-//c filter group (view-source:http://schteppe.github.io/cannon.js/demos/collisionFilter.html)
-//d density (ie mass) probably wont be that useful
-//e layer (create equally all around in different layers)
-//f light (chance of changing to heat)
-//g colors
-//h reflectivity  
-//i change in color based on heat
-//j axial tilt
-//k sleepiness
-//l size
-
-
-
-
-
-
 redFilter = 0x030000;
 greenFilter = 0x000300;
 blueFilter = 0x000003;
@@ -193,7 +39,21 @@ function updateHeat(body){
 
         
         
+		gravity =function(){
 
+            // Get the vector pointing from the moon to the planet center
+            var moon_to_planet = new CANNON.Vec3();
+            this.position.negate(moon_to_planet);
+
+            // Get distance from planet to moon
+            var distance = moon_to_planet.norm();
+
+            // Now apply force on moon
+            // Fore is pointing in the moon-planet direction
+            moon_to_planet.normalize();
+            moon_to_planet.mult(1500/Math.pow(distance,2),this.force);
+            updateHeat(this);
+        }
         
 
 
@@ -215,7 +75,7 @@ function updateHeat(body){
       demo.addScene("Moon",function(){
           world = demo.getWorld();
           world.solver.iterations = 1;
-          world.allowSleep = true;
+          //world.allowSleep = true;
            
           
           var lightBaseShape = new CANNON.Sphere(0.5);
@@ -401,31 +261,6 @@ function updateHeat(body){
         });
 
       demo.start();
-      
-      var data = {
-    		    labels: [""],
-    		    datasets: [
-    		        {
-    		            label: "My First dataset",
-    		            fillColor: "rgba(220,220,220,0.5)",
-    		            strokeColor: "rgba(220,220,220,0.8)",
-    		            highlightFill: "rgba(220,220,220,0.75)",
-    		            highlightStroke: "rgba(220,220,220,1)",
-    		            data: [0]
-    		        }
-    		        ,
-    		        {
-    		            label: "My Second dataset",
-    		            fillColor: "rgba(151,187,205,0.5)",
-    		            strokeColor: "rgba(151,187,205,0.8)",
-    		            highlightFill: "rgba(151,187,205,0.75)",
-    		            highlightStroke: "rgba(151,187,205,1)",
-    		            data: [0]
-    		        }
-    		    ]
-    		};
-    		var ctx = document.getElementById("myChart").getContext("2d");
-    		var myBarChart = new Chart(ctx).Bar(data, {});
       differenceA = []
       interval = setInterval(function(){
     	dataPos = {}  
---
+++ climatesimulation/oldexamples/4/test4.js
@@ -0,0 +1,317 @@
+//world.broadphase = new CANNON.SAPBroadphase();
+//world.broadphase = new CANNON.GridBroadphase();
+
+setInterval(function() {
+	demo.settings.stepFrequency = 120
+}, 1)
+setInterval(function() {
+	demo.settings.stepFrequency = 160
+}, 1)
+
+
+var useLight = true;
+mPerLight = 30
+lightMass = .00001
+
+randomAllOver = function(shape) {
+	currentBody.position.set(Math.random() * 5, Math.random() * 6 - 3, Math
+			.random() * 6 - 3);
+}
+randomNone = function() {
+}
+
+gravity = function() {
+
+	// Get the vector pointing from the moon to the planet center
+	var moon_to_planet = new CANNON.Vec3();
+	this.position.negate(moon_to_planet);
+
+	// Get distance from planet to moon
+	var distance = moon_to_planet.norm();
+
+	// Now apply force on moon
+	// Fore is pointing in the moon-planet direction
+	moon_to_planet.normalize();
+	moon_to_planet.mult(15000 / Math.pow(distance, 2), this.force);
+
+}
+
+types = {}
+function addType(type, obj) {
+	types[type] = obj
+}
+
+function useType(args) {
+	var obj = new CANNON.Body({
+		mass : args.mass
+	});
+	obj.addShape(args.shape);
+	if (args.gravity === undefined || args.gravity)
+		obj.preStep = gravity
+	obj.allowSleep = true;
+	obj.sleepSpeedLimit = args.sleepSpeed;// Body will feel sleepy if speed<1
+	// (speed == norm of velocity)
+	obj.sleepTimeLimit = args.sleepTimeLimit; // Body falls asleep after 1s of
+	// sleepiness
+	world.add(obj);
+	if (args.visibility)
+		demo.addVisual(obj, new THREE.MeshLambertMaterial({
+			color : args.color
+		}));
+	currentBody = obj;
+}
+layers = []
+function addLayer(obj) {
+	layers.push(obj);
+}
+layerRun = function(args, amount) {
+	if (amount == -1) {
+		setTimeout(this.layerRun.bind(this, args, 0), args.start);
+		return;
+	}
+	amount++;
+	useType(types[args.type])
+	args.random(args.params)
+	if (amount < args.amount)
+		setTimeout(this.layerRun.bind(this, args, amount), args.timeBetween)
+
+}
+function useLayer() {
+	for (idx in layers) {
+		layerRun(layers[idx], -1);
+
+	}
+
+}
+
+var atmosphereShape = new CANNON.Sphere(1);
+var smallShape = new CANNON.Sphere(0.4);
+var lightShape = new CANNON.Sphere(0.05);
+var planetShape = new CANNON.Sphere(1.5);
+var particleShape = new CANNON.Particle();
+
+addType("planet", {
+	"gravity" : false,
+	"visibility" : true,
+	"color" : 0x49311C,
+	"shape" : planetShape,
+	"mass" : 500000,
+	"sleepSpeed" : .1,
+	"sleepTimeLimit" : 1
+})
+
+addType("dirt", {
+	"gravity" : true,
+	"visibility" : true,
+	"color" : 0x49311C,
+	"shape" : smallShape,
+	"mass" : 5,
+	"sleepSpeed" : .1,
+	"sleepTimeLimit" : 1
+})
+
+addType("greenery", {
+	"gravity" : true,
+	"visibility" : true,
+	"color" : 0x00FF00,
+	"shape" : smallShape,
+	"mass" : 5,
+	"sleepSpeed" : .1,
+	"sleepTimeLimit" : 1
+})
+
+addType("ocean", {
+	"gravity" : true,
+	"visibility" : true,
+	"color" : 0x0000FF,
+	"shape" : smallShape,
+	"mass" : 5,
+	"sleepSpeed" : .1,
+	"sleepTimeLimit" : 1
+})
+
+addType("atmosphere", {
+	"gravity" : true,
+	"visibility" : false,
+	"color" : null,
+	"shape" : smallShape,
+	"mass" : 5,
+	"sleepSpeed" : .1,
+	"sleepTimeLimit" : 1
+})
+
+addType("clouds", {
+	"gravity" : true,
+	"visibility" : true,
+	"color" : 0xFFFFFF,
+	"shape" : smallShape,
+	"mass" : 5,
+	"sleepSpeed" : .1,
+	"sleepTimeLimit" : 1
+})
+// addType("greenery",gravity, true, 0x00FF00, particleShape, 1, .1, 1)
+// addType("ocean",gravity, true, 0x0000FF, particleShape, 1, .1, 3)
+// addType("atmosphere",gravity, false, null, atmosphereShape, .1, .1, 3)
+// addType("clouds",gravity, true, 0xFFFFFF, particleShape, 1, .1, 3)
+
+addLayer({
+	"type" : "planet",
+	"start" : 0,
+	"timeBetween" : 0,
+	"amount" : 1,
+	"random" : randomNone,
+})
+addLayer({
+	"type" : "dirt",
+	"start" : 30,
+	"timeBetween" : 30,
+	"amount" : 10,
+	"random" : randomAllOver,
+	"params" : {
+		"shape" : 3
+	}
+})
+
+addLayer({
+	"type" : "greenery",
+	"start" : 200,
+	"timeBetween" : 30,
+	"amount" : 30,
+	"random" : randomAllOver,
+	"params" : {
+		"shape" : 3
+	}
+})
+
+addLayer({
+	"type" : "ocean",
+	"start" : 600,
+	"timeBetween" : 30,
+	"amount" : 50,
+	"random" : randomAllOver,
+	"params" : {
+		"shape" : 3
+	}
+})
+addLayer({
+	"type" : "atmosphere",
+	"start" : 2000,
+	"timeBetween" : 30,
+	"amount" : 600,
+	"random" : randomAllOver,
+	"params" : {
+		"shape" : 3
+	}
+})
+
+addLayer({
+	"type" : "clouds",
+	"start" : 10000,
+	"timeBetween" : 30,
+	"amount" : 10,
+	"random" : randomAllOver,
+	"params" : {
+		"shape" : 3
+	}
+})
+var demo = new CANNON.Demo();
+
+demo.addScene("Moon", function() {
+	world = demo.getWorld();
+	// world.broadphase = new CANNON.SAPBroadphase();
+	world.solver.iterations = 1;
+	world.allowSleep = true;
+	// world.allowSleep = false;
+
+	var lightBaseShape = new CANNON.Sphere(0.5);
+	var lightBase = new CANNON.Body({
+		mass : 5
+	});
+	lightBase.addShape(lightBaseShape);
+
+	lightBase.position.set(6, 0, 0);
+	lightBase.velocity.set(0, 0, 8);
+	// lightBase.velocity.set(0,0,4);
+	lightBase.linearDamping = 0.0;
+	lightBase.preStep = gravity
+	world.add(lightBase);
+	demo.addVisual(lightBase, new THREE.MeshLambertMaterial({
+		color : "0xFFFF00"
+	}));
+	useLayer();
+	bodies = []
+	toRemove = []
+
+	interval = setInterval(function() {
+		if (!useLight)
+			return;
+		var moon = new CANNON.Body({
+			mass : lightMass
+		});
+		moon.addShape(new CANNON.Particle());
+		// moon.addShape(lightShape);
+
+		// moon.position.set(Math.random() * 30,Math.random() *
+		// 10-4,Math.random() * 10-4);
+		// moon.position.set(Math.random() * 30,Math.random() *
+		// 8-4,Math.random() * 8-4);
+		// moon.position.set(Math.random() * 30,Math.random() *
+		// 6-3,Math.random() * 6-3);
+		// moon.position.set(Math.random() *
+		// 15+lightBase.position.x,Math.random() *
+		// 6-3+lightBase.position.y,Math.random() * 4-2+lightBase.position.z);
+		moon.position.set(Math.random() + lightBase.position.x, Math.random()
+				+ lightBase.position.y, Math.random() + lightBase.position.z);
+		// moon.velocity.set(0,0,8);
+		// moon.velocity.set(-8,0,0);
+
+		// moon.linearDamping = 0.0;
+
+		world.add(moon);
+
+		demo.addVisual(moon, new THREE.MeshLambertMaterial({
+			color : 0xffff00
+		}));
+		bodies.push(moon)
+
+		var moon_to_planet = new CANNON.Vec3();
+		moon.position.negate(moon_to_planet);
+		var distance = moon_to_planet.norm();
+		moon_to_planet.normalize();
+		moon_to_planet.mult(80, moon.velocity);
+
+		if (bodies.length > 100) {
+			var b = bodies.shift();
+			demo.removeVisual(b);
+			world.remove(b);
+		}
+
+		moon.addEventListener("collide", function(e) {
+
+			if (Math.random() < .5) {
+				var body
+				if (e.contact.bi.heatDistribute)
+					body = e.contact.bi
+				else
+					body = e.body
+				toRemove.push(this);
+				body.heat += 20
+				// updateHeat(body);
+			}
+
+		});
+		for (b in toRemove) {
+			var pos = toRemove[b].position;
+			demo.removeVisual(toRemove[b]);
+			world.remove(toRemove[b]);
+
+		}
+		toRemove.length = 0;
+
+	}, mPerLight);
+
+	hDA = []
+
+});
+
+demo.start();
