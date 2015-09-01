d945455d443c20c511e377b24c8278e3e0355e12
terra <terra@terra-pc.exe>
2015-09-01 02:26:50 -0400

    earth (almost)

--- climatesimulation/test/loadingImage.js
+++ climatesimulation/test/loadingImage.js
@@ -10,18 +10,18 @@ var myCanvas = document.getElementById('earthCanvas');
 	  myCanvas.width = img.width;
 	  ctx.drawImage(img,0,0); // Or at whatever offset you like
 	  radius = 10;	  
-	  for(b = 0; b < myCanvas.height; b+=25){
-		var widthIncrement = Math.abs(myCanvas.height/2 - b)
+	  for(b = 50; b < myCanvas.height-50; b+=30){
+		var widthIncrement = Math.cos(2 * Math.PI *b/myCanvas.height) * 20
 //		
 //		widthIncrement = widthIncrement * 50
 //		
 //		
-//		widthIncrement = Math.abs(Math.floor(widthIncrement))
+		widthIncrement = 40+ widthIncrement
 //		if(widthIncrement < 10){
 //			widthIncrement = 10
 //		}
 //		widthIncrement = 50
-	  	for(a = 0; a < myCanvas.width; a+=25+ widthIncrement){
+	  	for(a = 0; a < myCanvas.width; a+=widthIncrement){
 	  		
 			var pixelData = ctx.getImageData(a, b, 1, 1).data;
 			intermediate.push([a,b,[pixelData[0],pixelData[1],pixelData[2]]])
